75e88e2e8230de072c7efc4afce65f10e8be0263
PedroVitor-oss <82410794+PedroVitor-oss@users.noreply.github.com>
2024-03-18 14:59:36 -0300

    Add Menu in game

--- Assets/Scripts/menu/MenuManage.cs
+++ Assets/Scripts/menu/MenuManage.cs
@@ -1,22 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 using UnityEngine.SceneManagement;
 
 
 public class MenuManage : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private GameObject MenuOpitions;
+    [SerializeField] private GameObject Confimation;
+    [SerializeField] private string NextLevel;
+
+
     public void Jogar(){
-        SceneManager.LoadScene("Level1");
+        SceneManager.LoadScene(NextLevel);
     }
-    public void Opitions(){
-        SceneManager.LoadScene("Opitions");
+    public void OpenOptions(){
+        MenuOpitions.SetActive(true);
+    }
+    public void CloseOptions(){
+        MenuOpitions.SetActive(false);
+    }
+    public void OpenConfirm(){
+        Confimation.SetActive(true);
+    }
+    public void CloseConfirm(){
+        Confimation.SetActive(false);
     }
     public void Inicio(){
         SceneManager.LoadScene("Menu");
     }
     public void Fechar(){
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
          Application.Quit();
     }
 }
